364dbf16f3b353bc81c707833198a9c723621a3e
Joey <adelajajoseph04@gmail.com>
2025-12-13 10:27:59 +0100

    Fixed some write ups

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Brillo",
+  title: "Brillo | Design Agency",
   description: "Creative Branding Agency",
 };
 
